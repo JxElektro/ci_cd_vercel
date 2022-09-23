662d5ec2e076d74b26a59d7dd2d274111e4f8af7
Jhen Nunez <jxelektro@gmail.com>
2022-09-22 21:56:46 -0300

    1p. Vitel Set -  Main.yaml Set -

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,10 @@
+import react from 'react';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+
+
+test('Renders main page correctly', () => {
+  render(<App />)
+  expect(true).toBeTruthy();
+});
